3b0a796927ee28987f5a6c1afa109a908e2ee921
Артур Хожасаитов <92801959+XanZavoevatel@users.noreply.github.com>
2023-01-11 18:57:04 +0300

    аватар готов

--- app/src/main/java/ru/lncloud/projecta/MainActivity.kt
+++ app/src/main/java/ru/lncloud/projecta/MainActivity.kt
@@ -2,7 +2,10 @@ package ru.lncloud.projecta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         buttonSearch.setOnClickListener {
             Toast.makeText(this@MainActivity, "This is search", Toast.LENGTH_LONG).show()
         }
+
+        val linerLayout: ViewGroup = findViewById(R.id.liner_layout)
+
+        val avatarContent = layoutInflater.inflate(R.layout.content, linerLayout, false)
+        val content = avatarContent.findViewById<ViewGroup>(R.id.content)
+        val image: ImageView = content.findViewById(R.id.image_content)
+        image.setImageResource(R.drawable.avatar)
+        val textView: TextView = content.findViewById(R.id.text_content)
+        textView.text = "Avatar!"
+        linerLayout.addView(avatarContent)
+
     }
+
+
 }
