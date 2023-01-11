53308ced2ba4fc1171fc0ff40abc686a6408f395
Артур Хожасаитов <92801959+XanZavoevatel@users.noreply.github.com>
2023-01-11 15:21:11 +0300

    верхнее меню готово

--- app/src/main/java/ru/lncloud/projecta/MainActivity.kt
+++ app/src/main/java/ru/lncloud/projecta/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.lncloud.projecta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonMenu: Button = findViewById(R.id.button_top_menu)
+        buttonMenu.setOnClickListener {
+            Toast.makeText(this@MainActivity, "This is menu", Toast.LENGTH_LONG).show()
+        }
+
+        val buttonSearch: Button = findViewById(R.id.button_search)
+        buttonSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "This is search", Toast.LENGTH_LONG).show()
+        }
     }
 }
