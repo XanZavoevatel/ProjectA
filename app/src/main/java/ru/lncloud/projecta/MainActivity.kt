82d163f0c6d46e0e0803bc49b77c065feca926b1
Артур Хожасаитов <92801959+XanZavoevatel@users.noreply.github.com>
2023-01-15 12:22:51 +0300

    проект готов

--- app/src/main/java/ru/lncloud/projecta/MainActivity.kt
+++ app/src/main/java/ru/lncloud/projecta/MainActivity.kt
@@ -22,16 +22,37 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "This is search", Toast.LENGTH_LONG).show()
         }
 
-        val linerLayout: ViewGroup = findViewById(R.id.liner_layout)
+        val buttonBuyTicket: Button = findViewById(R.id.buy_ticket)
+        buttonBuyTicket.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Ticket purchased", Toast.LENGTH_LONG).show()
+        }
 
-        val avatarContent = layoutInflater.inflate(R.layout.content, linerLayout, false)
-        val content = avatarContent.findViewById<ViewGroup>(R.id.content)
-        val image: ImageView = content.findViewById(R.id.image_content)
+        val contentList: ViewGroup = findViewById(R.id.liner_layout)
+
+        val avatarContent = layoutInflater.inflate(R.layout.content, contentList, false)
+        val avatarViewGroup = avatarContent.findViewById<ViewGroup>(R.id.content)
+        val image: ImageView = avatarViewGroup.findViewById(R.id.image_content)
         image.setImageResource(R.drawable.avatar)
-        val textView: TextView = content.findViewById(R.id.text_content)
-        textView.text = "Avatar!"
-        linerLayout.addView(avatarContent)
+        val textView: TextView = avatarViewGroup.findViewById(R.id.text_content)
+        textView.text = resources.getString(R.string.avatar)
 
+        val spartaContent = layoutInflater.inflate(R.layout.content,contentList, false)
+        val spartaViewGroup = spartaContent.findViewById<ViewGroup>(R.id.content)
+        val imageSparta: ImageView = spartaContent.findViewById(R.id.image_content)
+        imageSparta.setImageResource(R.drawable.sparta)
+        val textSparta: TextView = spartaViewGroup.findViewById(R.id.text_content)
+        textSparta.text = resources.getString(R.string.sparta)
+
+        val returnOfTheKingContent = layoutInflater.inflate(R.layout.content,contentList, false)
+        val returnOfTheKingViewGroup = returnOfTheKingContent.findViewById<ViewGroup>(R.id.content)
+        val imageReturnOfTheKing: ImageView = returnOfTheKingViewGroup.findViewById(R.id.image_content)
+        imageReturnOfTheKing.setImageResource(R.drawable.returnoftheking_1)
+        val textReturnOfTheKing: TextView = returnOfTheKingViewGroup.findViewById(R.id.text_content)
+        textReturnOfTheKing.text = resources.getString(R.string.lord_ring)
+
+        contentList.addView(avatarContent)
+        contentList.addView(spartaContent)
+        contentList.addView(returnOfTheKingContent)
     }
 
 
