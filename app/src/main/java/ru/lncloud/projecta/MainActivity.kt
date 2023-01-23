9649e4d7a70ad654f9a7f63d180ea805f822fdf5
Артур Хожасаитов <92801959+XanZavoevatel@users.noreply.github.com>
2023-01-23 16:05:20 +0300

    clean code

--- app/src/main/java/ru/lncloud/projecta/MainActivity.kt
+++ app/src/main/java/ru/lncloud/projecta/MainActivity.kt
@@ -1,9 +1,8 @@
 package ru.lncloud.projecta
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import ru.lncloud.projecta.dao.MovieInfo
 import ru.lncloud.projecta.databinding.ActivityMainBinding
 import ru.lncloud.projecta.utils.MovieListAdapter
@@ -35,40 +34,16 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.buyTicket.setOnClickListener {
             Toast.makeText(this@MainActivity, "Ticket purchased", Toast.LENGTH_LONG).show()
         }
-
-//        val contentList: ViewGroup = findViewById(R.id.liner_layout)
-//
-//        val avatarContent = layoutInflater.inflate(R.layout.content, contentList, false)
-//        val avatarViewGroup = avatarContent.findViewById<ViewGroup>(R.id.content)
-//        val image: ImageView = avatarViewGroup.findViewById(R.id.image_content)
-//        image.setImageResource(R.drawable.avatar)
-//        val textView: TextView = avatarViewGroup.findViewById(R.id.text_content)
-//        textView.text = resources.getString(R.string.avatar)
-//
-//        val spartaContent = layoutInflater.inflate(R.layout.content,contentList, false)
-//        val spartaViewGroup = spartaContent.findViewById<ViewGroup>(R.id.content)
-//        val imageSparta: ImageView = spartaContent.findViewById(R.id.image_content)
-//        imageSparta.setImageResource(R.drawable.sparta)
-//        val textSparta: TextView = spartaViewGroup.findViewById(R.id.text_content)
-//        textSparta.text = resources.getString(R.string.sparta)
-//
-//        val returnOfTheKingContent = layoutInflater.inflate(R.layout.content,contentList, false)
-//        val returnOfTheKingViewGroup = returnOfTheKingContent.findViewById<ViewGroup>(R.id.content)
-//        val imageReturnOfTheKing: ImageView = returnOfTheKingViewGroup.findViewById(R.id.image_content)
-//        imageReturnOfTheKing.setImageResource(R.drawable.returnoftheking_1)
-//        val textReturnOfTheKing: TextView = returnOfTheKingViewGroup.findViewById(R.id.text_content)
-//        textReturnOfTheKing.text = resources.getString(R.string.lord_ring)
-//
-//        contentList.addView(avatarContent)
-//        contentList.addView(spartaContent)
-//        contentList.addView(returnOfTheKingContent)
     }
 
-    fun setInitialData() {
+    private fun setInitialData() {
         listMovies.add(MovieInfo(R.drawable.avatar, resources.getString(R.string.avatar)))
         listMovies.add(MovieInfo(R.drawable.sparta, resources.getString(R.string.sparta)))
-        listMovies.add(MovieInfo(R.drawable.returnoftheking_1, resources.getString(R.string.lord_ring)))
+        listMovies.add(
+            MovieInfo(
+                R.drawable.returnoftheking_1,
+                resources.getString(R.string.lord_ring)
+            )
+        )
     }
-
-
 }
